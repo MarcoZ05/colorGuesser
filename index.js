58eb8fad6ce19f1207411dc878b79841df1f77c7
Käkäsh <77887301+MarcoZ05@users.noreply.github.com>
2022-10-09 19:24:39 +0200

    train only works once...

--- index.js
+++ index.js
@@ -1,9 +1,5 @@
 import externData from "./data.js";
 
-// create and train the neural network
-const net = new brain.NeuralNetwork();
-net.train(externData);
-
 // init html elements
 const block1 = document.getElementById("block1");
 const block2 = document.getElementById("block2");
@@ -17,12 +13,20 @@ const trainForm = document.getElementById("trainForm");
 const acceptButton = document.getElementById("accept");
 const rejectButton = document.getElementById("reject");
 
+// init neural network
+var net = new brain.NeuralNetwork();
+
 // last result and dataset
 let result = "";
 let dataset = {};
 
+// all training data
+const trainingData = externData;
+
 colorForm.addEventListener("submit", (e) => {
   e.preventDefault();
+
+  net.train(externData);
 
   testDataset();
   toggleForm();
@@ -31,7 +35,8 @@ colorForm.addEventListener("submit", (e) => {
 acceptButton.addEventListener("click", (e) => {
   e.preventDefault();
 
-  net.train([{ input: dataset, output: { [result]: 1 } }]);
+  trainingData.push({ input: dataset, output: { [result]: 1 } });
+
   toggleForm();
 });
 
@@ -40,17 +45,19 @@ rejectButton.addEventListener("click", (e) => {
 
   result = prompt("What color is it?");
   setBackgroud(dataset, result);
-  net.train([{ input: dataset, output: { [result]: 1 } }]);
-  console.log(net.toJSON());
+
+  trainingData.push({ input: dataset, output: { [result]: 1 } });
 
   toggleForm();
 });
 
 function testDataset() {
-  dataset = getDataset()
+  dataset = getDataset();
 
   result = brain.likely(dataset, net);
   setBackgroud(dataset, result);
+
+  net = new brain.NeuralNetwork();
 }
 
 function toggleForm() {
@@ -59,8 +66,9 @@ function toggleForm() {
 }
 
 function setBackgroud() {
-  block1.style.backgroundColor = `hsl(${dataset.hue * 360}, ${dataset.saturation * 100
-    }%, ${dataset.lightness * 100}%)`;
+  block1.style.backgroundColor = `hsl(${dataset.hue * 360}, ${
+    dataset.saturation * 100
+  }%, ${dataset.lightness * 100}%)`;
   block2.style.backgroundColor = result;
 }
 
