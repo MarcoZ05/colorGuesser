5fe9e53b717c5bb79ebe30eaff3e393f7a09951b
Käkäsh <marco.zillgen23@gmail.com>
2022-10-09 20:03:34 +0200

    fdtdfc

--- data.js
+++ data.js
@@ -1,13 +1,13 @@
 export default [
   { input: { hue: 0, saturation: 0, lightness: 0 }, output: { black: 1 } },
-  { input: { hue: 0, saturation: 0, lightness: 1 }, output: { white: 1 } },
-  { input: { hue: 0, saturation: 1, lightness: 0.5 }, output: { red: 1 } },
-  {
-    input: { hue: 1 / 3, saturation: 1, lightness: 0.5 },
-    output: { green: 1 },
-  },
-  {
-    input: { hue: 2 / 3, saturation: 1, lightness: 0.5 },
-    output: { blue: 1 },
-  },
+  // { input: { hue: 0, saturation: 0, lightness: 1 }, output: { white: 1 } },
+  // { input: { hue: 0, saturation: 1, lightness: 0.5 }, output: { red: 1 } },
+  // {
+  //   input: { hue: 1 / 3, saturation: 1, lightness: 0.5 },
+  //   output: { green: 1 },
+  // },
+  // {
+  //   input: { hue: 2 / 3, saturation: 1, lightness: 0.5 },
+  //   output: { blue: 1 },
+  // },
 ];
